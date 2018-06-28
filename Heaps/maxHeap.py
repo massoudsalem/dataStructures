7117e013b2d9b394b2f82f0bbd57fcc3624d1f21
massoudsalem <massoudsalem@users.noreply.github.com>
2018-06-28 09:47:25 +0200

    max-lists-str
    Adding maxHeap structure.
    Adding list constractor.
    Removing printing method.
    Adding string representation.

--- Heaps/maxHeap.py
+++ Heaps/maxHeap.py
@@ -1,7 +1,11 @@
-# This MinHeap structure is written by Moh.Massoud in 28Jan18
+# This MaxHeap structure is written by Moh.Massoud in 28Jan18
 class Heap:
-    def __init__(self):
-        self.heap = []
+    def __init__(self,list=[]):
+        self.heap=[]
+        for i in list:
+            self.insertNode(i)
+    def __str__(self):
+        return "{}".format(self.heap)
     def size(self):
         return len(self.heap) if len(self.heap) else print("The heap doesn't contain any elements.")
     def top(self):
@@ -16,20 +20,20 @@ class Heap:
         return int((node-1)/2) if node!=0 else -1
     def reHeapUp(self,node):
         parent=self.getParent(node)
-        if node == 0 or self.heap[parent]<self.heap[node]:
+        if node == 0 or self.heap[parent]>self.heap[node]:
             return
         self.heap[parent],self.heap[node]=self.heap[node],self.heap[parent]
         self.reHeapUp(parent)
     def reHeapDown(self,node):
-        minChild=self.getLeftChild(node)
-        if minChild==-1:
+        maxChild=self.getLeftChild(node)
+        if maxChild==-1:
             return
         rightChild=self.getRightChild(node)
-        if self.heap[minChild]>self.heap[rightChild]:
-            minChild=rightChild
-        if self.heap[node]>self.heap[minChild]:
-            self.heap[minChild], self.heap[node] = self.heap[node], self.heap[minChild]
-            self.reHeapDown(minChild)
+        if self.heap[maxChild]<self.heap[rightChild]:
+            maxChild=rightChild
+        if self.heap[node]<self.heap[maxChild]:
+            self.heap[maxChild], self.heap[node] = self.heap[node], self.heap[maxChild]
+            self.reHeapDown(maxChild)
     def insertNode(self,node):
         self.heap.append(node)
         self.reHeapUp(self.size()-1)
@@ -38,8 +42,8 @@ class Heap:
             self.heap[0]=self.heap[-1]
             self.heap.pop()
             self.reHeapDown(0)
-    def printHeap(self):
-        print(self.heap)
+
+
 
 h=Heap()
 h.insertNode(2)
@@ -51,22 +55,26 @@ h.insertNode(36)
 h.insertNode(7)
 h.insertNode(25)
 h.insertNode(100)
-h.printHeap()
+print(h)
 h.removeTop()
-h.printHeap()
+print(h)
 h.removeTop()
-h.printHeap()
+print(h)
 h.removeTop()
-h.printHeap()
+print(h)
 h.removeTop()
-h.printHeap()
+print(h)
 h.removeTop()
-h.printHeap()
+print(h)
 h.removeTop()
-h.printHeap()
+print(h)
 h.removeTop()
-h.printHeap()
+print(h)
 h.removeTop()
-h.printHeap()
+print(h)
 h.removeTop()
-h.printHeap()
+print(h)
+y=[12,3,45]
+print(y)
+y=Heap(y)
+print(y)
